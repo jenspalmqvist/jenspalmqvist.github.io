0d1b826ebcb81984a96114b66e26a51cbcb55ea2
Jens Palmqvist <jpalmqvi@gmail.com>
2024-01-10 11:14:07 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// "use strict";
+
+{
+  const coolElement = document.getElementById("coolElement");
+  name2 = "Jens";
+  console.log(name2);
+  function onClick() {
+    if (coolElement.innerHTML === "Jens") {
+      chainFunction();
+    }
+  }
+
+  function chainFunction() {}
+}
